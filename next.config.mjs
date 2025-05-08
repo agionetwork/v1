73fbe36aadb76e9c7b4585884d9ad956b0c11024
Ravi Aymara <seu-email@exemplo.com>
2025-05-08 02:27:23 -0300

    fix: corrige problemas de build e segurança

--- next.config.mjs
+++ next.config.mjs
@@ -12,11 +12,6 @@ const nextConfig = {
     ],
     unoptimized: true,
   },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
     return config
@@ -24,7 +19,7 @@ const nextConfig = {
 }
 
 const withCivicAuth = createCivicAuthPlugin({
-  clientId: "65004c36-3e4f-41a1-b0eb-8a9fc72dbf04"
+  clientId: process.env.CIVIC_CLIENT_ID || "65004c36-3e4f-41a1-b0eb-8a9fc72dbf04"
 })
 
 export default withCivicAuth(nextConfig)
